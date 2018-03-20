aa60c6beca768207091938ebcb79b4e5ecfccbb8
Danielbsb <danielbsb2010@gmail.com>
2018-03-20 02:02:28 +0000

    update

---
+++ Questoes/teste.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (void){
+	char name[100];
+        printf("Qual é o seu nome ? \n");
+	scanf("%s", name);
+	printf("Olá %s ! \n", name); 
+return 0;
+}
+
